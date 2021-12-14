4f9f86d16a4997cbad050e3d85316d5eae70a83f
Julian Minder <julian.minder@icloud.com>
2021-12-14 20:33:37 +0100

    fixed issues with building package

--- rel2graph/__init__.py
+++ rel2graph/__init__.py
@@ -1,17 +1,6 @@
-__all__ = []
-
 from .core.factories.resource import Resource
-__all__.extend(["Resource"])
-
 from .core.converter import Converter
-__all__.extend(["Converter"])
-
 from .core.resource_iterator import ResourceIterator, IteratorIterator
-__all__.extend([
-    "ResourceIterator",
-    "IteratorIterator"
-])
-
 from .core.factories.registrar import (
                             register_attribute_postprocessor,
                             register_attribute_preprocessor,
@@ -19,14 +8,5 @@ from .core.factories.registrar import (
                             register_subgraph_preprocessor,
                             register_factory
                             )
-__all__.extend(["register_attribute_postprocessor",
-                            "register_attribute_preprocessor",
-                            "register_subgraph_postprocessor",
-                            "register_subgraph_preprocessor",
-                            "register_factory"])
-
 from .core.factories.matcher import Matcher
-__all__.extend(["Matcher"])
-
 from .core.graph_elements import Attribute
-__all__.extend(["Attribute"])
