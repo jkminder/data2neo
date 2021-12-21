6283589107b628605a4ce94cc01762a51c4b1911
Julian Minder <julian.minder@icloud.com>
2021-12-21 14:19:11 +0100

    updated version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ requirements = [
 setup(
     name = "rel2graph", 
     packages = find_packages(),
-    version = "0.1.1",
+    version = "0.2.1",
     description = "Library for converting relational data into graph data (neo4j)",
     author = "Julian Minder",
     author_email = "jminder@ethz.ch",
