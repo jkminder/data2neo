70dc2d61840ea35f34d91ae36a85e3fdaeee6a6f
Julian Minder <julian.minder@icloud.com>
2022-02-22 12:41:39 +0100

    updated DATE/DATETIME to also handle datetime inputs and strip away tz info, added mission tests

--- rel2graph/common_modules/util.py
+++ rel2graph/common_modules/util.py
@@ -7,12 +7,16 @@ authors: Julian Minder
 """
 from .. import register_attribute_postprocessor
 from .. import Attribute
-from datetime import datetime, date
+from datetime import datetime
 
 @register_attribute_postprocessor
 def DATETIME(attribute, format_string="%Y-%m-%dT%H:%M:%S"):
+    if isinstance(attribute.value, datetime):
+        return Attribute(attribute.key, attribute.value.replace(tzinfo=None))
     return Attribute(attribute.key, datetime.strptime(attribute.value, format_string))
 
 @register_attribute_postprocessor
 def DATE(attribute, format_string="%Y-%m-%dT%H:%M:%S"):
+    if isinstance(attribute.value, datetime):
+        return Attribute(attribute.key, attribute.value.replace(tzinfo=None).date())
     return Attribute(attribute.key, datetime.strptime(attribute.value, format_string).date())
---
+++ tests/unit/common_modules/test_util.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Tests for OData relational module
+
+TODO: tests heavily access protected members of the library -> improve? how?
+
+authors: Julian Minder
+"""
+
+import pytest
+
+from rel2graph.common_modules import util
+from rel2graph import Attribute
+from datetime import datetime
+
+@pytest.fixture
+def default_format():
+    return Attribute("key", "2015-05-17T21:18:19")
+
+@pytest.fixture
+def other_format():
+    return Attribute("key", "2015/05/17 21h 18min 19s")
+
+@pytest.fixture
+def as_datetime():
+    return Attribute("key", datetime.strptime("2015-05-17T21:18:19", "%Y-%m-%dT%H:%M:%S"))
+
+def test_date(default_format, other_format, as_datetime):
+    result = util.DATE(default_format).value
+    assert(result.year == 2015)
+    assert(result.month == 5)
+    assert(result.day == 17)
+    result = util.DATE(other_format, "%Y/%m/%d %Hh %Mmin %Ss").value
+    assert(result.year == 2015)
+    assert(result.month == 5)
+    assert(result.day == 17)
+    result = util.DATE(as_datetime).value
+    assert(result.year == 2015)
+    assert(result.month == 5)
+    assert(result.day == 17)
+
+def test_datetime(default_format, other_format, as_datetime):
+    result = util.DATETIME(default_format).value
+    assert(result.year == 2015)
+    assert(result.month == 5)
+    assert(result.day == 17)
+    assert(result.hour == 21)
+    assert(result.minute == 18)
+    assert(result.second == 19)
+    result = util.DATETIME(other_format, "%Y/%m/%d %Hh %Mmin %Ss").value
+    assert(result.year == 2015)
+    assert(result.month == 5)
+    assert(result.day == 17)
+    assert(result.hour == 21)
+    assert(result.minute == 18)
+    assert(result.second == 19)    
+    result = util.DATETIME(as_datetime).value
+    assert(result.year == 2015)
+    assert(result.month == 5)
+    assert(result.day == 17)
+    assert(result.hour == 21)
+    assert(result.minute == 18)
+    assert(result.second == 19)
