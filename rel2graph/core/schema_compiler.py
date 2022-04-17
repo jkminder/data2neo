81213115065707eca574a3c676af9c5714c94efe
Julian Minder <julian.minder@icloud.com>
2022-04-17 21:08:23 +0200

    First version of the new compiler

---
+++ rel2graph/core/schema_compiler.py
@@ -0,0 +1,405 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Classes and methods for parsing and compiling the config document into factories.
+
+A file is first read and precompiled (all comments removed).
+The string is then parsed with the ply yacc/lex implementation into instructions, which
+is a list of [modulename, attribute] pairs (where attributes can be instructions as well). Finally the instructions are then compiled into factories.
+
+authors: Julian Minder
+"""
+
+from typing import List, Any
+import re
+import logging
+from ply import lex, yacc
+
+from .factories.registrar import get_factory
+
+logger = logging.getLogger(__name__)
+
+
+class SchemaConfigException(ValueError):
+    pass
+
+
+class SchemaConfigParser:
+    """
+    Parses the schema config file into rel2graph instructions that can directly be converted into factories and their arguments. Uses ply to
+    parse and lex the grammar.
+    """
+
+    tokens = (
+        'INT',
+        'FLOAT',
+        'STRING',
+        'BOOL',
+        'ENTITY',
+        'NODE',
+        'RELATION',
+        'MATCH',
+        'NAME',
+        'DOT',
+        'COMMA',
+        'EQUAL',
+        'LPAR',
+        'RPAR',
+        'COLON',
+        'SEP',
+    )
+
+    t_STRING = r'"(?:(?!"|\\).|\\.)*"|\'(?:(?!\'|\\).|\\.)*\''
+    t_NAME = r'\b(?!\b(?:False|True|ENTITY|NODE|RELATION|MATCH)\b)[a-zA-Z_]\w*\b'
+    t_ENTITY = r'\bENTITY\b'
+    t_NODE = r'\bNODE\b'
+    t_RELATION = r'\bRELATION\b'
+    t_MATCH = r'\bMATCH\b'
+    t_DOT = r'\.(?!\d+\b)'
+    t_COMMA = r'\,'
+    t_EQUAL = r'\='
+    t_COLON = r'\:'
+    t_LPAR = r'\('
+    t_RPAR = r'\)'
+    t_SEP = r'[+-](?<!\w[+-])'
+
+    def t_BOOL(self, t):
+        r'\b(?:True|False)\b'
+        t.value = t.value == "True"
+        return t
+
+    def t_INT(self, t):
+        r'\b(?<!\.)\d+(?!\.)\b'
+        t.value = int(t.value)
+        return t
+
+    def t_FLOAT(self, t):
+        r'\b(?<!\.)\d+\.\d+\b'
+        t.value = float(t.value)
+        return t
+
+    # Define a rule so we can track line numbers
+    def t_newline(self, t):
+        r'\n+'
+        t.lexer.lineno += len(t.value)
+
+    # A string containing ignored characters (spaces and tabs)
+    t_ignore = ' \t'
+
+    # Error handling rule
+    def t_error(self, t):
+        raise SchemaConfigException(f"Illegal character '{t.value[0]}' on line {t.lexer.lineno}\nCONTEXT: \n...{t.lexer.lexdata[t.lexpos-20:t.lexpos]}{t.value[0]}\u0332{t.value[1:50]}\n...")
+
+    def __init__(self):
+        self.lexer = lex.lex(module=self)
+        self.parser = yacc.yacc(module=self, debug=False)
+
+    def tokenize(self, data):
+        self.lexer.input(data)
+        while True:
+            tok = self.lexer.token()
+            if not tok:
+                break      # No more input
+            print(tok)
+
+    def parse(self, data):
+        return self.parser.parse(data)
+
+    def p_entities(self, p):
+        '''entities : entity entities
+                    | empty'''
+        case = len(p)-1
+        instructions = []
+        if case == 2:
+            instructions.append(p[1])
+            if len(p[2]):
+                instructions.extend(p[2])
+        p[0] = instructions
+
+    def p_entity(self, p):
+        '''entity : ENTITY LPAR STRING RPAR COLON graphelements
+        '''
+        p[0] = [self._cleanup_string(p[3]), p[6]]
+
+    @staticmethod
+    def _inject_graphelement_args(instructions, attributes, identifier):
+        """
+        This function injects graphelement arguments (attributes, primary_key and identifier) into a list of instructions containing a graphelement. Due to
+        possible wrapper definitions this requires recusively diving into the instructions. Further it returns a flag specifying if this graphelement represents a
+        node.
+
+        Returns:
+            Instructions with injected arguments, flag if this is a node
+        """
+        if instructions[0] in ["NodeFactory", "RelationFactory"]:
+            is_node = instructions[0] == "NodeFactory"
+            # Find primary attribute and extract attributes
+            raw_attributes = []
+            primary_key = None
+            for primary, attribute in attributes:
+                raw_attributes.append(attribute)
+                if primary:
+                    if primary_key is not None:
+                        raise SchemaConfigException(
+                            f"Setting two or more primary keys for one graphelement is not allowed. Conflict: '{primary_key}' <-> '{attribute[1][0]}'")
+                    primary_key = attribute[1][0]
+            instructions[1].insert(0, raw_attributes)
+            instructions[1].extend([primary_key, identifier])
+        else:
+            instructions[1][0], is_node = SchemaConfigParser._inject_graphelement_args(
+                instructions[1][0], attributes, identifier)
+        return instructions, is_node
+
+    def p_graphelements(self, p):
+        '''graphelements : graphelement identifier COLON attributes graphelements
+                         | empty'''
+        case = len(p)-1
+        instructions = [[], []]  # nodes, relations
+        if case == 5:
+            # We need to inject the attributes into the correct location in the graph element instructions
+            # The attributes are always the first argument for any graphelement
+            graphelement, is_node = SchemaConfigParser._inject_graphelement_args(p[1], p[4], p[2])
+            instructions[0 if is_node else 1].append(graphelement)
+
+            instructions[0].extend(p[5][0])
+            instructions[1].extend(p[5][1])
+
+        p[0] = instructions
+
+    def p_identifier(self, p):
+        '''identifier : NAME
+                      | empty'''
+        p[0] = p[1]
+
+    def p_graphelement(self, p):
+        '''graphelement : node
+                        | relation
+                        | NAME LPAR graphelement staticarguments RPAR'''
+        case = len(p)-1
+        instructions = []
+        if case == 5:
+            instructions = [p[1], [p[3], *p[4]]]
+        else:
+            instructions = p[1]
+        p[0] = instructions
+
+    def p_node(self, p):
+        '''node : NODE LPAR arguments RPAR'''
+        p[0] = ["NodeFactory", [p[3]]]
+
+    def p_relation(self, p):
+        '''relation : RELATION LPAR destination COMMA argument COMMA destination RPAR'''
+        p[0] = ["RelationFactory", [p[5], p[3], p[7]]]
+
+    def p_destination(self, p):
+        '''destination : NAME
+                       | MATCH LPAR mixedarguments RPAR'''
+        case = len(p)-1
+        instructions = []
+        if case == 1:
+            instructions = ["Matcher", [p[1]]]
+        else:
+            instructions = ["Matcher", [None, *p[3]]]
+        p[0] = instructions
+
+    def p_staticvalue(self, p):
+        '''staticvalue : STRING
+                       | BOOL
+                       | INT
+                       | FLOAT'''
+        p[0] = SchemaConfigParser._cleanup_string(p[1])
+
+    def p_attributes(self, p):
+        '''attributes : SEP attribute attributes
+                      | empty'''
+        case = len(p)-1
+        instructions = []
+        if case == 3:
+            instructions = [(True if p[1] == '+' else False, p[2])]
+            instructions.extend(p[3])
+
+        p[0] = instructions
+
+    @staticmethod
+    def _inject_attribute_key(instructions, key):
+        """
+        This function injects attribute arguments (attribute key) into a list of instructions containing a attribute. Due to
+        possible wrapper definitions this requires recusively diving into the instructions.
+        """
+        if instructions[0] in ["AttributeFactory"]:
+            instructions[1][0] = key
+        else:
+            instructions[1][0] = SchemaConfigParser._inject_attribute_key(
+                instructions[1][0], key)
+        return instructions
+
+    def p_attribute(self, p):
+        '''attribute : NAME EQUAL argument'''
+        instructions = p[3]
+        # Set attribute key (requires recursion due to possible wrappers)
+        SchemaConfigParser._inject_attribute_key(instructions, p[1])
+        p[0] = instructions
+
+    def p_staticarguments(self, p):
+        '''staticarguments : COMMA staticvalue staticarguments
+                           | empty'''
+        p[0] = []
+        if len(p) > 2:
+            p[0].append(["AttributeFactory", [None, None, p[2]]])
+            p[0].extend(p[3])
+
+    def p_arguments(self, p):
+        '''arguments : argument
+                     | argument COMMA arguments'''
+        p[0] = [p[1]]
+        if len(p) > 2:
+            p[0].extend(p[3])
+
+    def p_argument(self, p):
+        '''argument : staticvalue
+                    | NAME DOT NAME
+                    | NAME LPAR argument staticarguments RPAR'''
+        case = len(p) - 1
+        if case == 1:
+            p[0] = ["AttributeFactory", [None, None, p[1]]]
+        elif case == 3:
+            p[0] = ["AttributeFactory", [None, p[3], None]]
+        elif case == 5:
+            p[0] = [p[1], [p[3], *p[4]]]
+
+    def p_mixedarguments(self, p):
+        '''mixedarguments : argument
+                          | attribute
+                          | argument COMMA mixedarguments
+                          | attribute COMMA mixedarguments'''
+        p[0] = [p[1]]
+        if len(p) > 2:
+            p[0].extend(p[3])
+
+    def p_empty(self, p):
+        '''empty : '''
+
+    def p_error(self, p):
+        raise Exception(p)
+
+    @staticmethod
+    def _cleanup_string(value):
+        """
+        Removes parentheses from string if value is of instance string.
+        This is a required hack as the direct regex rule doesn't work here (don't know why).
+        """
+        if isinstance(value, str):  # hack to remove quotes, direct match didnt work
+            return value[1:-1]
+        return value
+
+
+def _precompile(input_string):
+    # Remove all comments
+    match = re.search("#.*", input_string)
+    while match is not None:
+        input_string = input_string[:match.start()] + \
+            input_string[match.end():]
+        match = re.search("#.*", input_string)
+    return input_string
+
+
+def _unravel_args(args: List[Any]) -> List[Any]:
+    """Unravels arguments by compiling them if required.
+
+    Args:
+        args: List of argument instructions
+    Returns:
+        A compiled list of arguments
+    """
+    unraveled = []
+    for arg in args:
+        if isinstance(arg, list):
+            unraveled.append(_compile_instructions(arg))
+        else:
+            unraveled.append(arg)
+    return unraveled
+
+
+def _compile_module(module: str, args: List) -> "Factory":
+    """Compiles a module and args into a factory"""
+    factory_args = _unravel_args(args)
+    logger.debug(f"Compiling module '{module}' with static args {[arg for arg in factory_args if isinstance(arg, str)]}")
+    return get_factory(module)(*factory_args)
+
+
+def _compile_instructions(instructions: List) -> "Factory":
+    """Recursively compiles a list of instructions
+
+    Args:
+        instructions: The instructions to compile
+    Returns:
+        A compiled factory
+    """
+    # Check if for single or nested module
+    if len(instructions) == 0:
+        return []
+    if isinstance(instructions[0], str):
+        module, args = instructions
+        return _compile_module(module, args)
+
+    factories = []
+    for module, args in instructions:
+        factories.append(_compile_module(module, args))
+    return factories
+
+
+def compile_schema(schema_config_file: str) -> List["Factory"]:
+    """Parses a config schema file into usable factories.
+
+    Args:
+        filename: Filepath of the schema config.
+    Returns:
+        A dict in form of (entity_type_name, (NodeSupplyChain, RelationSupplyChain))
+        for all provided entity_types.
+    """
+    logger.debug(f"Parsing config file '{schema_config_file}'")
+    with open(schema_config_file, "r", encoding="utf-8") as fstream:
+        config_schema_string = fstream.read()
+
+    # Removes comments
+    precompiled_string = _precompile(config_schema_string)
+
+    parser = SchemaConfigParser()
+    instructions = parser.parse(precompiled_string)
+    compiled = {}
+
+    for entity_type, entity_instructions in instructions:
+        if entity_type in compiled.keys():
+            raise SchemaConfigException("Found two conflicting definitions of entity '{entity_type}'. Please only specify each entity once.")
+        node_instructions, relation_instructions = entity_instructions
+        node_factories, relation_factories = _compile_instructions(node_instructions), _compile_instructions(relation_instructions)
+        compiled[entity_type] = (get_factory("SupplyChain")(node_factories, "NodeSupplyChain"),
+                                 get_factory("SupplyChain")(relation_factories, "RelationSupplyChain"))
+    return compiled
+
+
+
+if __name__ == "__main__":
+    parser = SchemaConfigParser()
+
+    precompiled_string = """
+    ENTITY("entity"):
+    WRAPPER(NODE("label", WRAP("label2"), WRAP("label3", 1234), entity.column), "someargument", 123) nodeid:
+        + test = entity.column
+        - test1 = "static \\" string"
+        - test2 = WRAP2(WRAP(entity.col))
+    RELATION(MATCH("label", "label2", name="test", id=WRAP(test.idcolumn)), "type", to):
+        + test = entity.column
+        - test1 = "static \\" string"
+        - test2 = WRAP2(WRAP(entity.col))
+    ENTITY("second"):
+        RELATION(MATCH("label", "label2", name="test", id=WRAP(test.idcolumn)), "type", to):
+        + test = entity.column
+        - test1 = "static \\" string"
+        - test2 = WRAP2(WRAP(entity.col))
+    ENTITY("third"):
+    """
+    precompiled_string = _precompile(precompiled_string)
+    parser.tokenize(precompiled_string)
+    print(parser.parse(precompiled_string))
---
+++ tests/unit/core/test_schema_compiler.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Tests for OData relational module
+
+TODO: tests heavily access protected members of the library -> improve? how?
+
+authors: Julian Minder
+"""
+
+import pytest
+
+from rel2graph.core.schema_compiler import compile_schema, SchemaConfigParser, _precompile
+from rel2graph import register_attribute_preprocessor
+
+@register_attribute_preprocessor
+def WRAPPER(resource):
+    return resource
+
+def get_rel_type(relation_factory):
+    return relation_factory._type.static_attribute_value
+
+def get_labels(node_factory):
+    return [af.static_attribute_value for af in node_factory._labels]
+
+def af2str(list_of_af):
+    """attribute factories to strings"""
+    return [(af.attribute_key, (af.entity_attribute if af.entity_attribute is not None else af.static_attribute_value)) for af in list_of_af]
+
+def get_filepath(name):
+    return f"tests/unit/core/resources/{name}.yaml"
+
+def test_full_compiler_matcher_conditions():
+    """Tests if conditions of matcher (dynamic and static) are parsed and compiled correctly"""
+    relation_supplychain = compile_schema(get_filepath("matcher_condition"))["entity"][1] # get relation supplychain
+    for rf in relation_supplychain.factories:
+        type =  get_rel_type(rf) 
+        assert type in ["static-dyn", "static", "two-static", "dyn", "two-dyn", "two-dyn-two-static"]
+        # check from matcher
+        fm = rf._from_matcher
+        assert fm._node_id == "identifier"
+        # check to matcher with static conditions
+        tm = rf._to_matcher
+        assert (None, "entity") in af2str(tm._labels) # check labels
+        if "static" in type:
+            assert ("Name", "static") in af2str(tm._conditions) # check first static condition
+        if "two-static" in type:
+            assert ("Name2", "static2") in af2str(tm._conditions) # check second static condition
+        if "dyn" in type:
+            assert ("Dyn", "dyn") in af2str(tm._conditions) # check first dyn condition
+        if "two-dyn" in type:
+            assert ("Dyn2", "dyn2") in af2str(tm._conditions) # check second dyn condition
+        
+def test_full_compiler_node_primary():
+    """Test if primary keys for nodes are correct parsed"""
+    node_supplychain, _ = compile_schema(get_filepath("primary_keys"))["entity"]
+     
+    for nf in node_supplychain.factories:
+        labels = get_labels(nf)
+        assert(len(labels) == 1)
+        label = labels[0]
+        assert(label in ["noattr", "nopk", "pk"])
+        if label in ["noattr", "nopk"]:
+            assert(nf._primary_key is None)
+        if label in ["pk"]:
+            assert(nf._primary_key == "pk")
+
+def test_full_compiler_relations_primary():
+    """Test if primary keys for relations are correct parsed"""
+    _, relation_supplychain = compile_schema(get_filepath("primary_keys"))["entity"]
+     
+    for rf in relation_supplychain.factories:
+        type =  get_rel_type(rf) 
+
+        assert(type in ["noattr", "nopk", "pk"])
+        if type in ["noattr", "nopk"]:
+            assert(rf._primary_key is None)
+        if type in ["pk"]:
+            assert(rf._primary_key == "pk")
+
+def check_types(list_of_attribuets):
+    for (key, value) in list_of_attribuets:
+            if key == "myint":
+                assert(isinstance(value, int) and value==1)
+            elif key == "mystr":
+                assert(isinstance(value, str) and value=="1")
+            elif key == "myfloat":
+                assert(isinstance(value, float) and value==1.1)
+            elif key == "myTrue":
+                assert(isinstance(value, bool) and value)
+            elif key == "myFalse":
+                assert(isinstance(value, bool) and not value)
+            else:
+                assert(False) # not possible
+
+def test_full_compiler_typing():
+    """Test if different types for static arguments are correctly parsed"""
+    node_supplychain, relation_supplychain = compile_schema(get_filepath("typing"))["entity"]
+
+    for nf in node_supplychain.factories:
+        attributes = af2str(nf._attributes)
+        check_types(attributes)
+
+    for rf in relation_supplychain.factories:
+        # check to matcher
+        fm = rf._to_matcher
+        conditions = af2str(fm._conditions)
+        check_types(conditions)
+
+def test_full_compiler_dynkeys():
+    node_supplychain, relation_supplychain = compile_schema(get_filepath("dynamic_keys"))["entity"]
+    
+    for nf in node_supplychain.factories:
+        for label in nf._labels:
+            assert(label._entity_attribute == "dynamic_key")
+        for attr in nf._attributes:
+            assert(attr._entity_attribute == "dynamic_key")
+    
+    for rf in relation_supplychain.factories:
+        assert(rf._type._entity_attribute == "dynamic_key")
+        for attr in nf._attributes:
+            assert(attr._entity_attribute == "dynamic_key")
+        
+def test_full_compiler_empty_entity():
+    node_supplychain, relation_supplychain = compile_schema(get_filepath("empty_entity"))["entity"]
+    assert len(node_supplychain.factories) == 0 
+    assert len(relation_supplychain.factories) == 0
+
+def test_parser():
+    input_string = """
+    ENTITY("entity"):
+    WRAPPER(NODE("label", WRAP("label2"), WRAP("label3", 1234), entity.column), "someargument", 123) nodeid:
+        + test = entity.column
+        - test1 = "static \\" string"
+        - test2 = WRAP2(WRAP(entity.col))
+    RELATION(MATCH("label", "label2", name="test", id=WRAP(test.idcolumn)), "type", to):
+        + test = entity.column
+        - test1 = "static \\" string"
+        - test2 = WRAP2(WRAP(entity.col))
+    ENTITY("second"):
+        RELATION(MATCH("label", "label2", name="test", id=WRAP(test.idcolumn)), "type", to):
+        + test = entity.column
+        - test1 = "static \\" string"
+        - test2 = WRAP2(WRAP(entity.col))
+    ENTITY("third"):
+    """
+    true_instructions = [['entity', [[['WRAPPER', [['NodeFactory', [[['AttributeFactory', ['test', 'column', None]], ['AttributeFactory', ['test1', None, 'static \\" string']], ['WRAP2', [['WRAP', [['AttributeFactory', ['test2', 'col', None]]]]]]], [['AttributeFactory', [None, None, 'label']], ['WRAP', [['AttributeFactory', [None, None, 'label2']]]], ['WRAP', [['AttributeFactory', [None, None, 'label3']], ['AttributeFactory', [None, None, 1234]]]], ['AttributeFactory', [None, 'column', None]]], 'test', 'nodeid']], ['AttributeFactory', [None, None, 'someargument']], ['AttributeFactory', [None, None, 123]]]]], [['RelationFactory', [[['AttributeFactory', ['test', 'column', None]], ['AttributeFactory', ['test1', None, 'static \\" string']], ['WRAP2', [['WRAP', [['AttributeFactory', ['test2', 'col', None]]]]]]], ['AttributeFactory', [None, None, 'type']], ['Matcher', [None, ['AttributeFactory', [None, None, 'label']], ['AttributeFactory', [None, None, 'label2']], ['AttributeFactory', ['name', None, 'test']], ['WRAP', [['AttributeFactory', ['id', 'idcolumn', None]]]]]], ['Matcher', ['to']], 'test', None]]]]], ['second', [[], [['RelationFactory', [[['AttributeFactory', ['test', 'column', None]], ['AttributeFactory', ['test1', None, 'static \\" string']], ['WRAP2', [['WRAP', [['AttributeFactory', ['test2', 'col', None]]]]]]], ['AttributeFactory', [None, None, 'type']], ['Matcher', [None, ['AttributeFactory', [None, None, 'label']], ['AttributeFactory', [None, None, 'label2']], ['AttributeFactory', ['name', None, 'test']], ['WRAP', [['AttributeFactory', ['id', 'idcolumn', None]]]]]], ['Matcher', ['to']], 'test', None]]]]], ['third', [[], []]]]
+    parser = SchemaConfigParser()
+    assert true_instructions == parser.parse(input_string)
+
+def test_precompile():
+    input_string = """
+    ENTITY("entity"):
+    WRAPPER(NODE("label", WRAP("label2"), WRAP("label3", 1234), entity.column), "someargument", 123) nodeid: # some comment
+        + test = entity.column #######
+        - test1 = "static \\" string" ##ka asdflkjasdölfj
+        - test2 = WRAP2(WRAP(entity.col))
+    RELATION(MATCH("label", "label2", name="test", id=WRAP(test.idcolumn)), "type", to):
+        + test = entity.column # testi 123 vier fünf
+        - test1 = "static \\" string" 
+        - test2 = WRAP2(WRAP(entity.col))
+    # this is another comment ,!'_
+    ENTITY("second"):
+        RELATION(MATCH("label", "label2", name="test", id=WRAP(test.idcolumn)), "type", to):
+        + test = entity.column # this is a coomment
+        - test1 = "static \\" string"
+        - test2 = WRAP2(WRAP(entity.col))
+    ENTITY("third"):
+    """
+    precompiled_string = """
+    ENTITY("entity"):
+    WRAPPER(NODE("label", WRAP("label2"), WRAP("label3", 1234), entity.column), "someargument", 123) nodeid: 
+        + test = entity.column 
+        - test1 = "static \\" string" 
+        - test2 = WRAP2(WRAP(entity.col))
+    RELATION(MATCH("label", "label2", name="test", id=WRAP(test.idcolumn)), "type", to):
+        + test = entity.column 
+        - test1 = "static \\" string" 
+        - test2 = WRAP2(WRAP(entity.col))
+    
+    ENTITY("second"):
+        RELATION(MATCH("label", "label2", name="test", id=WRAP(test.idcolumn)), "type", to):
+        + test = entity.column 
+        - test1 = "static \\" string"
+        - test2 = WRAP2(WRAP(entity.col))
+    ENTITY("third"):
+    """
+    assert _precompile(input_string) == precompiled_string 
